cef12789a88b4034acd4db2df70ddb815d7f80c0
Barbara Makowiec <barbara.makowiec@wp.pl>
2019-04-07 21:59:26 +0200

    add DevTools

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
 import reducers from '../reducers/index';
-
-const store = createStore(reducers);
+import DevTools from '../DevTools';
+const store = createStore(reducers, DevTools.instrument());
 
 export default store;
